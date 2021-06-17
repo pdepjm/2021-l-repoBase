6b9016d516e01810df949e5f7616ade7dd93c960
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2021-06-16 23:27:04 -0300

    programa de prueba - tests ok

---
+++ programa.pl
@@ -0,0 +1,25 @@
+entrenador(ash).
+entrenador(misty).
+entrenador(brock).
+entrenador(guido).
+entrenador(diana).
+entrenador(alf).
+
+tieneA(ash, pikachu).
+tieneA(ash, pidgeot).
+tieneA(ash, charizard).
+tieneA(brock, gyarados).
+tieneA(misty, charizard).
+tieneA(guido, carpincho).
+tieneA(diana, meowth).
+
+
+:- begin_tests(entrenador).
+
+test("Alguien que es entrenador", nondet):-
+    entrenador(diana).
+
+test("Alguien que no es entrenador", fail):-
+    entrenador(rasta).
+
+:- end_tests(entrenador).
